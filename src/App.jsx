cbb8cc30d7c7d0fec2e19e9270604dddbb5d38f6
baysarov-web <155926464+baysarov-web@users.noreply.github.com>
2024-03-13 21:02:16 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { fakeDatabase } from "./fakeDatabase";
+
+function App() {
+  const [challenges, setChallenges] = useState(fakeDatabase.challenges);
+
+  const handleUpdateComplexity = (id, type) => {
+    const challengesArray = challenges.map((item) => {
+      if (type === "minus") {
+        if(item.complexity > 0){
+          if (item.id === id) {
+            item.complexity--;
+          }
+        }
+      }
+      if (type === "plus") {
+        if(item.complexity < 100){
+          if (item.id === id) {
+            item.complexity++;
+          }
+        }
+      }
+      return item
+    });
+    setChallenges(challengesArray)
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6">
+          <div className="bg-light text-center p-2 display-6 mb-2">
+            Сложные чалленги
+          </div>
+          <div>
+            <table className="table table-striped">
+              <tbody>
+                <tr>
+                  <th>№</th>
+                  <th>Название</th>
+                  <th>Сложность</th>
+                </tr>
+              </tbody>
+              <tbody>
+                {challenges.map((challenge, index) => {
+                  return (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td className="w-75">{challenge.name}</td>
+                      <td>
+                        <div className="d-flex">
+                          <button
+                            className="btn btn-sm btn-outline-primary"
+                            onClick={() =>
+                              handleUpdateComplexity(challenge.id, "minus")
+                            }
+                          >
+                            -
+                          </button>
+                          <div style={{ minWidth: 50, textAlign: "center" }}>
+                            {challenge.complexity}
+                          </div>
+                          <button
+                            className="btn btn-sm btn-outline-primary"
+                            onClick={() =>
+                              handleUpdateComplexity(challenge.id, "plus")
+                            }
+                          >
+                            +
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/fakeDatabase.js
@@ -0,0 +1,24 @@
+export const fakeDatabase = {
+  challenges: [
+    {
+      id: 1,
+      name: "99 бутылок",
+      complexity: 2,
+    },
+    {
+      id: 2,
+      name: "генератор пароля",
+      complexity: 7,
+    },
+    {
+      id: 3,
+      name: "бэкэнд для магазина",
+      complexity: 0,
+    },
+    {
+      id: 4,
+      name: "текущий челлендж",
+      complexity: 100,
+    },
+  ],
+};
